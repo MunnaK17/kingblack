126e5cc2d1ae7adac8225b6471ddb1b5cb3e40b5
Obit Rahman <obitrahman7@gmail.com>
2026-05-11 16:50:34 +0700

    show raw errors for railway debugging

--- public/index.php
+++ public/index.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
@@ -14,7 +18,13 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+try {
+    /** @var Application $app */
+    $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+    $app->handleRequest(Request::capture());
+} catch (\Throwable $e) {
+    echo '<pre>';
+    echo $e;
+    echo '</pre>';
+}
